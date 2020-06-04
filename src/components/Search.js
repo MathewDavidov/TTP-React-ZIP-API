e2d017c6919ebecd6337f5d830afc827d7a71944
Bayardo Zelaya <bayardozelaya@gmail.com>
2020-06-04 15:34:09 -0400

    added setState to enter the values of the Data passed on

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,63 +1,79 @@
-import React, { Component } from 'react';
-import '../App.css';
-import Axios from 'axios';
+import React, { Component } from "react";
+import "../App.css";
+import Axios from "axios";
 
 class Search extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            zip: 10016
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      zip: 0,
+      city: "",
+      state: "",
+      longitude: "",
+      latitude: "",
+      population: 0,
+      totalWages: 0,
+    };
+  }
 
-    componentDidMount() {
-        const zip = this.state.zip;
-        const API_URL = `http://ctp-zip-api.herokuapp.com/zip/${zip}`;
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
 
-        Axios
-            .get(API_URL)
-            .then((response) => {
-                const data = response.data;
-                console.log(data);
-            })
-            .catch((error) => {
-                console.log(error)
-            });
-    }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const zip = this.state.zip;
+    const API_URL = `http://ctp-zip-api.herokuapp.com/zip/${zip}`;
 
-    componentDidUpdate() {
-        const zip = this.state.zip;
-
-        if (zip >= 10000 && zip <= 20000) {
-            const API_URL = `http://ctp-zip-api.herokuapp.com/zip/${zip}`;
-
-            Axios.get(API_URL)
-            .then((response) => {
-                const data = response.data;
-                console.log(data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-        } else {
-            console.log("Invalid ZIP");
-        }
-    }
-
-    handleChange = (e) => {
+    Axios.get(API_URL)
+      .then((response) => {
+        const data = response.data;
+        console.log(data);
         this.setState({
-          [e.target.name]: e.target.value
+          city: data[0].City,
+          state: data[0].State,
+          longitude: data[0].Long,
+          latitude: data[0].Lat,
+          population: data[0].EstimatedPopulation,
+          totalWages: data[0].TotalWages,
         });
-    }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    render() {
-        return (
-            <div className="container text-center">
-                Zip Code:
-                <input type="number" name="zip" id="inputNumber" onChange={this.handleChange}></input> 
+  render() {
+    return (
+      <div className="container text-center">
+        <form onSubmit={this.handleSubmit}>
+          <label>Zipcode:</label>
+          <input
+            type="number"
+            name="zip"
+            id="inputNumber"
+            onChange={this.handleChange}
+          ></input>
+          <output></output>
+        </form>
+        {this.state.city ? (
+          <div>
+            <h1>City:{this.state.city}</h1>
+            <div>State:{this.state.state}</div>
+            <div>
+              Location:({this.state.longitude} , {this.state.latitude})
             </div>
-        )
-    }
+            <div>Population:{this.state.population}</div>
+            <div>Total Wages:{this.state.totalWages}</div>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+    );
+  }
 }
 
 export default Search;
